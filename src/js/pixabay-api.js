bd932e99acd73da12514731e2f2cbbd4d0afdc71
Dima <dima@vantage.software>
2024-07-30 16:13:28 +0300

    fix api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 const API_KEY = '45156920-6e611072a4f9c22ae400d324b';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export const fetchImages = async (query) => {
   const params = new URLSearchParams({
